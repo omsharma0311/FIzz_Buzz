2e0f1b29718c6575adbf4578c1f01ec60b7e6221
OM SHARMA <osharma311@hotmail.com>
2022-10-20 16:45:48 +0530

    Add project files.

---
+++ FizzBuzz.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace FIzz_Buzz
+{
+    public class FizzBuzz
+    {
+        public string Calculate(int num)
+        {
+            if (num % 3 == 0 && num % 5 == 0)
+                return "FizzBuzz";
+            if (num % 3 == 0)
+                return "Fizz";
+            if (num % 5 == 0)
+                return "Buzz";
+            else
+                return "";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace FIzz_Buzz
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Number");
+
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            FizzBuzz obj= new FizzBuzz();
+            string result = obj.Calculate(number);
+
+            Console.WriteLine(result);
+        }
+    }
+}
